927bcbe4c3cae8a90a0a40ccfcd8f682c4ef2fc1
sivaprakasam <sivaprakasam@Siva>
2020-03-22 22:02:24 -0400

    First commit
    uploading all the files

---
+++ kafka-publisher/src/main/java/com/javatechie/spring/kafka/api/User.java
@@ -0,0 +1,41 @@
+package com.javatechie.spring.kafka.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class User {
+	
+	public void user()
+	{}
+	
+	private int id;
+	public User(int id, String name, String[] address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	private String name;
+	private String[] address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String[] getAddress() {
+		return address;
+	}
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+	
+
+}
